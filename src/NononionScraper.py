7ee5485937aaa70da7b46d1047c235c2aa1aff9f
Munz3526 <justincrain@hotmail.com>
2025-02-17 14:47:07 -0600

    Update NononionScraper.py

--- src/NononionScraper.py
+++ src/NononionScraper.py
@@ -2,7 +2,6 @@ import scrapy
 import re
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
-from scrapy.http import HtmlResponse
 
 class DarkWebScraper(scrapy.Spider):
     name = "dark_web_scraper"
@@ -14,39 +13,31 @@ class DarkWebScraper(scrapy.Spider):
     ]
 
     def __init__(self):
-options = Options()
-options.headless = True  # Run in headless mode
-profile_path = "/home/munz/snap/firefox/common/.mozilla/firefox/v0pl6ivt.Selenium-Profile"
-options.set_preference("profile", profile_path)
+        options = Options()
+        options.headless = True  # Run in headless mode
+        profile_path = "/home/munz/snap/firefox/common/.mozilla/firefox/v0pl6ivt.Selenium-Profile"
+        options.set_preference("profile", profile_path)
 
-driver = webdriver.Firefox(options=options)
+        self.driver = webdriver.Firefox(options=options)
 
     def parse(self, response):
-        """Extract usernames, emails, and crypto addresses."""
-        
-        self.driver.get(response.url)  # Load page in Selenium
+        self.driver.get(response.url)
         text_content = self.driver.page_source  # Get full page HTML
-        
-        # Regex for common data types
-        username_pattern = r"@[a-zA-Z0-9_]{3,15}"  # Example: @username
-        email_pattern = r"[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+"  # Emails
-        btc_address_pattern = r"\b[13][a-km-zA-HJ-NP-Z1-9]{25,34}\b"  # Bitcoin
-        eth_address_pattern = r"\b0x[a-fA-F0-9]{40}\b"  # Ethereum
 
-        # Find matches
+        username_pattern = r"@[a-zA-Z0-9_]{3,15}"
+        btc_address_pattern = r"\b[13][a-km-zA-HJ-NP-Z1-9]{25,34}\b"
+        eth_address_pattern = r"\b0x[a-fA-F0-9]{40}\b"
+
         usernames = re.findall(username_pattern, text_content)
-        emails = re.findall(email_pattern, text_content)
         btc_addresses = re.findall(btc_address_pattern, text_content)
         eth_addresses = re.findall(eth_address_pattern, text_content)
 
         yield {
             "url": response.url,
-            "usernames": list(set(usernames)),
-            "emails": list(set(emails)),
-            "btc_addresses": list(set(btc_addresses)),
-            "eth_addresses": list(set(eth_addresses)),
+            "usernames": usernames,
+            "btc_addresses": btc_addresses,
+            "eth_addresses": eth_addresses,
         }
 
     def closed(self, reason):
-        """Close Selenium driver when Scrapy is done."""
-        self.driver.quit()
+        self.driver.quit()  # Close Selenium driver when done
