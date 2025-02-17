52d46331a0038369285e96ec2176d7cfb15aae5c
Munz3526 <justincrain@hotmail.com>
2025-02-17 15:23:18 -0600

    Update NononionScraper.py

--- src/NononionScraper.py
+++ src/NononionScraper.py
@@ -1,33 +1,24 @@
 import scrapy
 import re
-from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
 
 class DarkWebScraper(scrapy.Spider):
     name = "dark_web_scraper"
-
     start_urls = [
         "https://news.ycombinator.com/",
         "https://www.bbc.com/news",
         "https://www.theverge.com/"
     ]
 
-    def __init__(self):
-        options = Options()
-        options.headless = True  # Run in headless mode
-        profile_path = "/home/munz/snap/firefox/common/.mozilla/firefox/m2fjug6h.Default User"
-        options.set_preference("profile", profile_path)
-
-        self.driver = webdriver.Firefox(options=options)
-
     def parse(self, response):
-        self.driver.get(response.url)
-        text_content = self.driver.page_source  # Get full page HTML
+        """Extract usernames and crypto addresses from web pages."""
+        text_content = response.text  # Get raw HTML as text
 
-        username_pattern = r"@[a-zA-Z0-9_]{3,15}"
-        btc_address_pattern = r"\b[13][a-km-zA-HJ-NP-Z1-9]{25,34}\b"
-        eth_address_pattern = r"\b0x[a-fA-F0-9]{40}\b"
+        # Regex for finding usernames and crypto addresses
+        username_pattern = r"@[a-zA-Z0-9_]{3,15}"  # Example: @username
+        btc_address_pattern = r"\b[13][a-km-zA-HJ-NP-Z1-9]{25,34}\b"  # Bitcoin
+        eth_address_pattern = r"\b0x[a-fA-F0-9]{40}\b"  # Ethereum
 
+        # Find matches
         usernames = re.findall(username_pattern, text_content)
         btc_addresses = re.findall(btc_address_pattern, text_content)
         eth_addresses = re.findall(eth_address_pattern, text_content)
@@ -38,6 +29,3 @@ class DarkWebScraper(scrapy.Spider):
             "btc_addresses": btc_addresses,
             "eth_addresses": eth_addresses,
         }
-
-    def closed(self, reason):
-        self.driver.quit() 
