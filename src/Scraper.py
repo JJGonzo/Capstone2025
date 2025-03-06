aebcbde49b85aef03e84b2394c1be78b61f77051
Munz3526 <justincrain@hotmail.com>
2025-03-06 10:23:34 -0600

    Update Scraper.py

--- src/Scraper.py
+++ src/Scraper.py
@@ -1,26 +1,3 @@
-'''
-MIT License
-Copyright (c) 2024 Josh Schiavone
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-'''
-
 __version__ = 3
 
 import sys
@@ -49,15 +26,6 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.probability import FreqDist
 from textblob import TextBlob
-
-
-notice = '''
-Note: 
-    This tool is not to be used for illegal purposes.
-    The author is not responsible for any misuse of Darkdump.
-    May God bless you all.
-    https://joshschiavone.com - https://github.com/josh0xA
-'''
 
 class Colors:
     W = '\033[0m'  # white 
