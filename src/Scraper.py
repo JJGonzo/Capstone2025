e1528ef4e3d5fa3b781c8f0ebaec361f972025c2
Munz3526 <justincrain@hotmail.com>
2025-03-06 12:23:32 -0600

    Update Scraper.py

--- src/Scraper.py
+++ src/Scraper.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.firefox.service import Service
 from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.common.by import By
